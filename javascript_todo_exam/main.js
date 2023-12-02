a3b3fa57d0c0c59f7585b22316c8f872d4226ee6
wendikardian <wendikardian@gmail.com>
2023-12-02 16:28:24 +0700

    updates

---
+++ javascript_todo_exam/main.js
@@ -0,0 +1,48 @@
+/**
+ * Color 1 : #607EAA
+ * Color 2 : #AC7088
+ * Color 3 : #EED180
+ * Color 4 : #F37878
+ * Color 5 : #90C8AC
+ * Color 6 : #D8CCA3
+ * Color 7 : #82A284
+ */
+
+
+let todoList = [
+    {
+        id: 1,
+        date: "2022-08-10",
+        time: "11:30",
+        todo: "Belanja Mingguan",
+        color: "#90C8AC"
+    },
+    {
+        id: 2,
+        date: "2022-08-10",
+        time: "10:30",
+        todo: "Memasak makanan",
+        color: "#F37878"
+    },
+    {
+        id: 3,
+        date: "2022-08-20",
+        time: "17:30",
+        todo: "Belajar Coding",
+        color: "#EED180"
+    },
+    {
+        id: 4,
+        date: "2022-08-13",
+        time: "14:30",
+        todo: "Bersih-bersih rumah",
+        color: "#82A284"
+    },
+    {
+        id: 5,
+        date: "2022-08-16",
+        time: "19:30",
+        todo: "Mencuci Baju",
+        color: "#607EAA"
+    }
+]
